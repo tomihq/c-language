3add6c824f17a34eeb53865d0bd6bd31d89c9cc6
tomihq <hernandeztomas584@gmail.com>
2025-08-25 02:22:58 -0300

    feat: add unsigned cases

--- 02-data-types/data-types.c
+++ 02-data-types/data-types.c
@@ -2,14 +2,20 @@
 
 int main(){
 	char c = 100;
+	unsigned char c2 = -100;
 	short s = -8712;
+	unsigned short s2 = -8712;
 	int i = 123456;
 	long l = 123456789;
+	unsigned long l2 = -123456789;
 	
 	printf("char(%lu): %d \n", sizeof(c), c);
 	printf("short(%lu): %d \n", sizeof(s), s);
 	printf("int(%lu): %d \n", sizeof(i), i);
 	printf("long(%lu): %ld \n", sizeof(l), l);
+	printf("unsigned char(%lu): %d \n", sizeof(c2), c2);
+	printf("unsigned short(%lu): %d \n", sizeof(s2), s2);
+	printf("unsigned long(%lu): %ld \n", sizeof(l2), l2);
 
 	return 0;
 
