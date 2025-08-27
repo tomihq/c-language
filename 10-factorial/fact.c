603066b7665ffdedc6fb4b1f6b6f28dc8f8cd223
tomihq <hernandeztomas584@gmail.com>
2025-08-26 23:31:52 -0300

    chore: sommee cool comments

--- 10-factorial/fact.c
+++ 10-factorial/fact.c
@@ -5,15 +5,16 @@ int fact(int n);
 
 /*won't work if n>=32*/
 int main(){
-	int32_t n = 0;
+	int32_t n = 0; //block-scope, automatic-duration
 	printf("Enter the value of n (must be a positive integer): ");
 	scanf("%d", &n);
-	signed long long r = fact(n);
+	signed long long r = fact(n); //block-scope, automatic-duration
 	printf("Fact n: %d is: %lld \n", n, r);
 	return 0;
 }
 
 /* n must be a positive number. */
+//global-scope, static-duration
 int fact(int n){
 	if(n == 0) return 1; 
 	return n * fact(n-1);
