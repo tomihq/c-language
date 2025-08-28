3a6d64090b68b760b2f55de05a3f9c736df32407
tomihq <hernandeztomas584@gmail.com>
2025-08-28 14:09:22 -0300

    add: init pointer to null

--- 14-void-pointers/voidpointers.c
+++ 14-void-pointers/voidpointers.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
 int main(){
-	void* ptr; //pointer to null, cannot be desreferenced (aka: access his value)
+	void* ptr = NULL; //pointer to null, cannot be desreferenced (aka: access his value)
+	
+	if(ptr == NULL){
+		printf("Pointer is null. Cannot be desreferenced \n");
+	}else{
+		printf("zz");
+	}
+	
 	int x = 42;
 	ptr = &x; //pointer to &x dir. address
-
+	printf("Pointer assigned to: %p\n", ptr);
 	//Convert the void pointer to an int* pointer to allow access to his value
 	int* p = (int*) ptr;
-	printf("Value: %d \n", *p);
+	printf("Pointer value: %d \n", *p);
 
 	return 0;
 }
