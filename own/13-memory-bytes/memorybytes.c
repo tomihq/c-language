9659d14e857ff02d5a29765f67ed5b8eafbe9dc7
tomihq <hernandeztomas584@gmail.com>
2025-08-30 02:42:30 -0300

    feat: clean

--- own/13-memory-bytes/memorybytes.c
+++ own/13-memory-bytes/memorybytes.c
@@ -2,14 +2,6 @@
 #include <stdint.h>
 
 int main(){
-	/*int8_t memoria[9] = {255, 31, 42, 0, 55, 67, -128, 127, -99}; 
-	const int32_t size = sizeof(memoria) / sizeof(memoria[0]);
-	uint8_t *x = (uint8_t*) memoria[0];
-	int8_t *y = (uint8_t*) memoria[size];
-
-	printf("Dir de x: %p Valor: %d\n", (void*) x, *x);
-	printf("Dir de y: %p Valor: %d\n", (void*) y, *y);
-	*/
 	int z = 42;
 	int *p = &z;
 
